df09a8a5f7d3234456b4552089bd623a9e580d98
andrei <andreins@ymail.com>
2015-10-20 17:07:11 +0100

    Fixed testing environment, added a few example of testing methods

--- app/src/androidTest/java/toucan/sunka/MainScreenTest.java
+++ app/src/androidTest/java/toucan/sunka/MainScreenTest.java
@@ -2,11 +2,11 @@ package toucan.sunka;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.EditText;
 
-import org.junit.Test;
 
 import java.security.KeyException;
 
@@ -15,40 +15,34 @@ import static org.junit.Assert.*;
 /**
  * Created by andrei on 19/10/15.
  */
+
+// Every test class extends ActivityInstrumentationTestCase2<MainScreen>
 public class MainScreenTest extends ActivityInstrumentationTestCase2<MainScreen> {
 
     MainScreen mainScreen;
+    int example;
 
+    // Constructor
     public MainScreenTest() {
         super(MainScreen.class);
-        mainScreen = getActivity();
     }
 
-    //
-    @Test
+    // Method used for initialization, calls itself before every Test method
+    // Method super.setUp() calls the constructor from ActivityInstrumentationTest
+    public void setUp() throws Exception{
+        super.setUp();
+        mainScreen = getActivity(); // Gets the activity under testing.
+                                    // Each test needs to have this called before!
+    }
+
+    //This is a basic test to see if JUnit is configured properly
     public void testScreenExists() {
         assertNotNull(mainScreen);
     }
 
-    // Function test template
-    public void testRandomMethod() throws Exception {
-        // Calling a method from main screen
-        int randomNumber = mainScreen.randomMethod();
-
-        // Pressing a button
-        Button singlePlayer = (Button) mainScreen.findViewById(R.id.singlePlayer);
-        TouchUtils.clickView(this, singlePlayer);
-
-        //Focusing a edit text
-        final EditText editText = null;
-
-        getInstrumentation().runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
-                editText.requestFocus();
-            }
-        });
-        getInstrumentation().waitForIdleSync();
-        getInstrumentation().sendStringSync("String");
+    // Testing the output of a method
+    public void testRandomMethod() {
+        example = mainScreen.randomMethod();
+        assertEquals(example, 5);
     }
 }
