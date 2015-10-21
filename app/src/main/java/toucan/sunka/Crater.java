a778e4c35d487f4411368940ca4333713252cb04
andrei <andreins@ymail.com>
2015-10-21 13:36:13 +0100

    Added Crater class

---
+++ app/src/main/java/toucan/sunka/Crater.java
@@ -0,0 +1,41 @@
+package toucan.sunka;
+
+import android.widget.Button;
+
+/**
+ * Created by andrei on 21/10/15.
+ */
+public class Crater {
+
+    //Player Owner;
+    Crater nextCrater, oppositeCrater;
+    int stones = 7; // Defaulted to 7
+
+    public Crater(){
+
+    }
+
+    public void placeAlong(int remainingStones){
+
+    }
+
+    public boolean isEmpty(){
+        return (stones == 0);
+    }
+
+    public Crater getNextCrater(){
+        return nextCrater;
+    }
+
+    public Crater getOppositeCrater(){
+        return oppositeCrater;
+    }
+
+    public void setNextCrater(Crater crater){
+        nextCrater = crater;
+    }
+
+    public void setOppositeCrater(Crater crater) {
+        oppositeCrater = crater;
+    }
+}
