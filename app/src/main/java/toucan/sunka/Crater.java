80f173680bdd964a9d8f424d41c18ddbf807edd7
Mert Ruso <mert.ruso@kcl.ac.uk>
2015-10-23 18:10:07 +0100

    Renamed Owner to owner in Crater class

--- app/src/main/java/toucan/sunka/Crater.java
+++ app/src/main/java/toucan/sunka/Crater.java
@@ -1,13 +1,11 @@
 package toucan.sunka;
 
-import android.widget.Button;
-
 /**
  * Created by andrei on 21/10/15.
  */
 public class Crater {
 
-    private Player Owner;
+    private Player owner;
     private Crater nextCrater, oppositeCrater;
     private int stones;
     private boolean store;
@@ -20,10 +18,10 @@ public class Crater {
     public void placeAlong(int remainingStones) {
         if (remainingStones != 0) {
             // Checks if next Crater is a not a store
-            if (!(nextCrater.isStore() && !Owner.isPlayingTurn())){
+            if (!(nextCrater.isStore() && !owner.isPlayingTurn())){
                 // Checks if it's last move & conditions are met to steal other players stones
-                if (remainingStones == 1 && nextCrater.isEmpty() && Owner.isPlayingTurn() && !nextCrater.isStore()) {
-                    Crater ownerStore = Owner.getStore();
+                if (remainingStones == 1 && nextCrater.isEmpty() && owner.isPlayingTurn() && !nextCrater.isStore()) {
+                    Crater ownerStore = owner.getStore();
                     ownerStore.setStones(nextCrater.getOppositeCrater().getStones() + ownerStore.getStones() + 1);
                     nextCrater.getOppositeCrater().setStones(0);
                     changeTurn();
@@ -59,15 +57,15 @@ public class Crater {
     {
         Player otherPlayer = nextCrater.getOppositeCrater().getOwner();
 
-        if (Owner.isPlayingTurn())
+        if (owner.isPlayingTurn())
         {
-            Owner.setPlayingTurnTo(false);
+            owner.setPlayingTurnTo(false);
             otherPlayer.setPlayingTurnTo(true);
         }
         else
         {
             otherPlayer.setPlayingTurnTo(false);
-            Owner.setPlayingTurnTo(true);
+            owner.setPlayingTurnTo(true);
         }
     }
 
@@ -89,7 +87,7 @@ public class Crater {
     }
 
     public Player getOwner(){
-        return Owner;
+        return owner;
     }
 
     public int getStones(){
@@ -109,6 +107,6 @@ public class Crater {
     }
 
     public void setOwner(Player player){
-        Owner = player;
+        owner = player;
     }
 }
