2704629dfa95397c5fceeb795b954bd80baeefc3
Andrei <andreins@ymail.com>
2015-10-25 01:06:02 +0000

    Polished code, added comments

--- app/src/main/java/toucan/sunka/Crater.java
+++ app/src/main/java/toucan/sunka/Crater.java
@@ -5,6 +5,7 @@ package toucan.sunka;
  */
 public class Crater {
 
+    public static final int ACTION_DELAY = 60;
     private Player owner;
     private Crater nextCrater, oppositeCrater;
     private int stones;
@@ -15,48 +16,56 @@ public class Crater {
         stones = this.store ? 0 : 7;
     }
 
+    /**
+     * Method first checks if the next crater is not a store.
+     *  If it isn't, it calls performMove()
+     *  Otherwise, it checks if the next store belongs to the active player
+     *    if so, it does a move.
+     *    Otherwise, it does the regular move, skipping the other player's store.
+     * @param stones represents how many stones are left to be placed
+     */
     public void placeAlong(int stones){
-        if ( stones != 0 ){
-            if (!nextCrater.isStore()){ //checks if it's not a store
-                if ( stones == 1 )  // checks if it's last stone
-                    stealOrMove(nextCrater); // recursive tree ends
-                else moveStone(nextCrater, stones - 1); //recursive call
+        if ( stones != 0 )
+            if (!nextCrater.isStore()) performMove(nextCrater, stones - 1);
+            else if ( belongsToActivePlayer(nextCrater) ) moveStone(nextCrater, stones - 1);
+                 else performMove(nextCrater.getNextCrater(), stones - 1);
+    }
+    /**
+     * Method first checks if the remaining stones are 0 - if it's the last move
+     *    if it is, it changes turn and it checks if it can perform a steal and does so.
+     *              if it can't perform a steal it does the last move,
+     *              and does NOT call placeAlong(0), which means the player does
+     *              NOT receive an extra turn
+     *    if it isn't the last move, it calls moveStone()
+     * @param crater is the next crater on which the stone will be added
+     * @param remainingStones is the number of remaining stones to be added
+     */
+    public void performMove(Crater crater, int remainingStones){
+        if ( remainingStones == 0 ) {
+            if ( crater.isEmpty() && belongsToActivePlayer(crater) ) {
+                Crater oppositeCrater = crater.getOppositeCrater();
+                Crater ownerStore = owner.getStore();
+                ownerStore.setStones(oppositeCrater.getStones() + ownerStore.getStones() + 1);
+                oppositeCrater.setStones(0);
             }
-            else { // it is a store
-                if ( belongsToCurrentPlayer(nextCrater) ) // checks if it's current player's store
-                    moveStone(nextCrater, stones - 1 ); // recursive call - if stones = 1 then the next call will be
-                                                         // moveStone( nextCrater, 0 ) which will signal the current player has an extra turn
-                else  // it is other player's store
-                    if ( stones == 1 ) stealOrMove(nextCrater.getNextCrater()); // recursive tree ends
-                    else moveStone(nextCrater.getNextCrater(), stones - 1);
-            }
+            else crater.setStones(crater.getStones() + 1);
+            changeTurn();
         }
+        else moveStone(crater, remainingStones); //recursive call
     }
 
-    public void stealOrMove(Crater crater) {
-        if ( crater.isEmpty() && belongsToCurrentPlayer(crater) ) //checks if you can steal
-            stealCrater(crater.getOppositeCrater());
-        else crater.setStones(crater.getStones() + 1); // does a normal move
-        changeTurn();
+    /**
+     * Adds 1 stone to the next crater and calls placeAlong()
+     * It can call placeAlong(0) which would mean the player would get an extra turn
+     * @param crater is the next crater on which the stone will be added
+     * @param remainingStones
+     */
+    public void moveStone(Crater crater, int remainingStones){
+        crater.setStones(crater.getStones() + 1);
+        crater.placeAlong(remainingStones);
     }
 
-    public boolean belongsToCurrentPlayer(Crater crater) {
-        return crater.getOwner().isPlayingTurn();
-    }
-
-    public void moveStone(Crater nextCrater, int remainingStones){
-        nextCrater.setStones(nextCrater.getStones() + 1);
-        nextCrater.placeAlong(remainingStones);
-    }
-
-    public void stealCrater(Crater oppositeCrater){
-        Crater ownerStore = owner.getStore();
-        ownerStore.setStones(oppositeCrater.getStones() + ownerStore.getStones() + 1);
-        oppositeCrater.setStones(0);
-    }
-
-    private void changeTurn()
-    {
+    private void changeTurn() {
         Player otherPlayer = nextCrater.getOwner().equals(owner) ?
                 nextCrater.getOppositeCrater().getOwner() :
                 nextCrater.getOwner();
@@ -73,8 +82,9 @@ public class Crater {
         }
     }
 
-    public boolean belongsTo(Player owner){
-        return nextCrater.getOwner().equals(owner);
+    // Checks if the crater belongs to the active player
+    public boolean belongsToActivePlayer(Crater crater) {
+        return crater.getOwner().isPlayingTurn();
     }
 
     public boolean isEmpty(){
