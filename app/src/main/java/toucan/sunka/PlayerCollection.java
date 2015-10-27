4a04959714a944725047831972f750c00885d313
MrKonstantinT <bocetozeleniq@abv.bg>
2015-10-27 15:13:38 +0000

    Removed a couple of unused imports.

--- app/src/main/java/toucan/sunka/PlayerCollection.java
+++ app/src/main/java/toucan/sunka/PlayerCollection.java
@@ -2,9 +2,7 @@ package toucan.sunka;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
