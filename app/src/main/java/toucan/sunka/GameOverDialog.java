c19b930ae7a01d9f9d4805b5800547d37e8f33d4
Musta Rohman <mustarohman@gmail.com>
2015-10-28 16:02:22 +0000

    Added onCreateDialog method to GameOverDialog class. Added TableLayout to the GameOver layout. Created table row layout for use in layoutInflator

--- app/src/main/java/toucan/sunka/GameOverDialog.java
+++ app/src/main/java/toucan/sunka/GameOverDialog.java
@@ -1,12 +1,28 @@
 package toucan.sunka;
 
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 
 /**
  * Created by mustarohman on 28/10/2015.
  */
 public class GameOverDialog extends DialogFragment {
 
+    @Override
+    public Dialog onCreateDialog(final Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.dialog_game_over, null));
 
 
+
+
+        return builder.create();
+
+    }
 }
