2f649d75d3219d070f5b62ab536b882f33473e21
MrKonstantinT <bocetozeleniq@abv.bg>
2015-11-13 14:27:54 +0000

    Wrote a getsFreeMove method.

--- app/src/main/java/toucan/sunka/SimpleAI.java
+++ app/src/main/java/toucan/sunka/SimpleAI.java
@@ -5,7 +5,15 @@ package toucan.sunka;
  */
 public class SimpleAI {
 
+    private int storeIndex;
+
     public SimpleAI() {
 
+    }
+
+    public boolean getsFreeMove(int craterIndex, int stones, int storeIndex) {
+        if (storeIndex == 8 && ((craterIndex + stones) - 1) == storeIndex) return true;
+        else if (storeIndex == 0 && ((craterIndex + stones) - 1) == 16) return true;
+        return true;
     }
 }
